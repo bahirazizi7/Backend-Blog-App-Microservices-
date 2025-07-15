16b1068baae251b46ef9c84ef7590f373f5074be
Bahir Azizi <bahirazizi7@gmail.com>
2025-07-15 16:36:42 +0430

    Quesry service added successfuly

---
+++ query/index.js
@@ -0,0 +1,34 @@
+const express= require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+const posts = {};
+app.post("/events", (req, res) => {
+  
+    const {type,data } = req.body;
+    if(type === "PostCreated") {
+        const { id, title } = data;
+        posts[id] = { id, title,comments: [] };
+
+        
+    }   
+    else if(type ==="CommentCreated") {
+        const { postId, id, content } = data;
+        const post = posts[postId];
+        post.comments.push({ id, content });
+    }
+    console.log(posts);
+    res.send({});
+
+})
+
+app.get("/posts", (req, res) => {
+  res.send(posts);
+}); 
+
+app.listen(4002, () => {
+  console.log('Server is running on port 4002');
+});
